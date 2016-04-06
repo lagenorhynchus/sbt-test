775935ab6690ac314166f52299005f555bf8ae14
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2016-04-06 16:35:15 +0900

    implement pattern matching exercises

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -102,3 +102,67 @@ object ListExercise {
 
   def count[T](xs: List[T])(f: T => Boolean): Int = xs.foldLeft(0)((acc, x) => if (f(x)) 1 + acc else acc)
 }
+
+
+// pattern matching
+object PatternMatchingExercise {
+  sealed abstract class DayOfWeek(val label: String)
+  case object Sunday extends DayOfWeek("日")
+  case object Monday extends DayOfWeek("月")
+  case object Tuesday extends DayOfWeek("火")
+  case object Wednesday extends DayOfWeek("水")
+  case object Thursday extends DayOfWeek("木")
+  case object Friday extends DayOfWeek("金")
+  case object Saturday extends DayOfWeek("土")
+
+  def nextDayOfWeek(d: DayOfWeek): DayOfWeek = d match {
+    case Sunday => Monday
+    case Monday => Tuesday
+    case Tuesday => Wednesday
+    case Wednesday => Thursday
+    case Thursday => Friday
+    case Friday => Saturday
+    case Saturday => Sunday
+  }
+
+  sealed trait Tree
+  case class Branch(value: Int, left: Tree, right: Tree) extends Tree
+  case object Empty extends Tree
+
+  def max(t: Tree): Int = t match {
+    case Empty => sys.error("Empty tree")
+    case Branch(v, Empty, Empty) => v
+    case Branch(v, Empty, r) => Math.max(v, max(r))
+    case Branch(v, l, Empty) => Math.max(v, max(l))
+    case Branch(v, l, r) => Seq(v, max(l), max(r)).max
+  }
+
+  def min(t: Tree): Int = t match {
+    case Empty => sys.error("Empty tree")
+    case Branch(v, Empty, Empty) => v
+    case Branch(v, Empty, r) => Math.min(v, min(r))
+    case Branch(v, l, Empty) => Math.min(v, min(l))
+    case Branch(v, l, r) => Seq(v, min(l), min(r)).min
+  }
+
+  def depth(t: Tree): Int = t match {
+    case Empty => 0
+    case Branch(_, l, r) => Seq(depth(l), depth(r)).max + 1
+  }
+
+  def sort(t: Tree): Tree = {
+    def toList(t: Tree): List[Int] = t match {
+      case Empty => Nil
+      case Branch(v, l, r) => toList(l) ++ List(v) ++ toList(r)
+    }
+    def toBinarySearchTree(ns: List[Int]): Tree = {
+      def insert(n: Int, t: Tree): Tree = t match {
+        case Empty => Branch(n, Empty, Empty)
+        case Branch(v, l, r) =>
+          if (n < v) Branch(v, insert(n, l), r) else Branch(v, l, insert(n, r))
+      }
+      ns.foldLeft(Empty: Tree)((acc, n) => insert(n, acc))
+    }
+    toBinarySearchTree(toList(t))
+  }
+}
